6341eeb0be9c744449ed1ed0768d33a14b63cc62
Hasan SEZER <vimevim@users.noreply.github.com>
2019-04-15 22:17:49 +0300

    Add project files.

---
+++ moveUs/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace moveUs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mousePos = new System.Windows.Forms.Label();
+            this.buttonPos = new System.Windows.Forms.Label();
+            this.btnBackSpace = new moveUs.ButtonElipse();
+            this.btnSpace = new moveUs.ButtonElipse();
+            this.btn8 = new moveUs.ButtonElipse();
+            this.btn3 = new moveUs.ButtonElipse();
+            this.btn2 = new moveUs.ButtonElipse();
+            this.btn1 = new moveUs.ButtonElipse();
+            this.btn4 = new moveUs.ButtonElipse();
+            this.btn0 = new moveUs.ButtonElipse();
+            this.btn5 = new moveUs.ButtonElipse();
+            this.btn6 = new moveUs.ButtonElipse();
+            this.btn7 = new moveUs.ButtonElipse();
+            this.SuspendLayout();
+            // 
+            // mousePos
+            // 
+            this.mousePos.AutoSize = true;
+            this.mousePos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mousePos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.mousePos.Location = new System.Drawing.Point(416, 61);
+            this.mousePos.Name = "mousePos";
+            this.mousePos.Size = new System.Drawing.Size(74, 17);
+            this.mousePos.TabIndex = 1;
+            this.mousePos.Text = "mousePos";
+            // 
+            // buttonPos
+            // 
+            this.buttonPos.AutoSize = true;
+            this.buttonPos.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonPos.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonPos.Location = new System.Drawing.Point(416, 102);
+            this.buttonPos.Name = "buttonPos";
+            this.buttonPos.Size = new System.Drawing.Size(72, 17);
+            this.buttonPos.TabIndex = 2;
+            this.buttonPos.Text = "buttonPos";
+            // 
+            // btnBackSpace
+            // 
+            this.btnBackSpace.Location = new System.Drawing.Point(320, 115);
+            this.btnBackSpace.Name = "btnBackSpace";
+            this.btnBackSpace.Size = new System.Drawing.Size(40, 120);
+            this.btnBackSpace.TabIndex = 11;
+            this.btnBackSpace.Text = "<";
+            this.btnBackSpace.UseVisualStyleBackColor = true;
+            this.btnBackSpace.Click += new System.EventHandler(this.btnBackSpace_Click);
+            this.btnBackSpace.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
+            this.btnBackSpace.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnBackSpace_MouseMove);
+            // 
+            // btnSpace
+            // 
+            this.btnSpace.Location = new System.Drawing.Point(115, 320);
+            this.btnSpace.Name = "btnSpace";
+            this.btnSpace.Size = new System.Drawing.Size(120, 40);
+            this.btnSpace.TabIndex = 10;
+            this.btnSpace.Text = "space";
+            this.btnSpace.UseVisualStyleBackColor = true;
+            this.btnSpace.Click += new System.EventHandler(this.btnSpace_Click);
+            this.btnSpace.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
+            this.btnSpace.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnSpace_MouseMove);
+            // 
+            // btn8
+            // 
+            this.btn8.FlatAppearance.BorderSize = 0;
+            this.btn8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn8.Location = new System.Drawing.Point(150, 150);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(50, 50);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "X";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            this.btn8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
+            this.btn8.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn3.Location = new System.Drawing.Point(220, 220);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(50, 50);
+            this.btn3.TabIndex = 7;
+            this.btn3.Text = "X";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn2.Location = new System.Drawing.Point(250, 150);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(50, 50);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "X";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn1.Location = new System.Drawing.Point(220, 80);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(50, 50);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn4.Location = new System.Drawing.Point(150, 250);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(50, 50);
+            this.btn4.TabIndex = 7;
+            this.btn4.Text = "X";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn0.Location = new System.Drawing.Point(150, 50);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(50, 50);
+            this.btn0.TabIndex = 7;
+            this.btn0.Text = "X";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn5.Location = new System.Drawing.Point(80, 220);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(50, 50);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn6.Location = new System.Drawing.Point(50, 150);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(50, 50);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "X";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn7.Location = new System.Drawing.Point(80, 80);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(50, 50);
+            this.btn7.TabIndex = 6;
+            this.btn7.Text = "X";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(175)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(350, 350);
+            this.Controls.Add(this.btnBackSpace);
+            this.Controls.Add(this.btnSpace);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.buttonPos);
+            this.Controls.Add(this.mousePos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label mousePos;
+        private System.Windows.Forms.Label buttonPos;
+        private ButtonElipse btn7;
+        private ButtonElipse btn0;
+        private ButtonElipse btn1;
+        private ButtonElipse btn6;
+        private ButtonElipse btn5;
+        private ButtonElipse btn4;
+        private ButtonElipse btn2;
+        private ButtonElipse btn3;
+        private ButtonElipse btn8;
+        private ButtonElipse btnSpace;
+        private ButtonElipse btnBackSpace;
+    }
+}
+
---
+++ moveUs/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace moveUs
+{
+    public partial class Form1 : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.ExStyle |= 0x08000000;
+                return param;
+            }
+        }//Klavye kodları.
+
+        int firstStep = 5;//ilk basamağımız
+        int secondStep = 10;//ikinci basamağımız
+        //5*8 dizi oluşturuyorum
+        string[,] keyPad = new string[5, 8] {
+            {"a","b","c","d","e","f","g","h"},
+            {"i","j","k","l","m","n","o","p"},
+            {"q","r","s","t","u","v","w","x"},
+            {"y","z","1","2","3","4","5","6"},
+            {"7","8","9","0",".",",","!","?"}
+        };
+        bool i = false;
+
+        private void writer(int birinciDeger, int ikinciDeger)
+        {//burada gerçekten iyi bir şey kullan ve bu kod hammallığından kurtul.
+            if (i == false)
+            {
+                btn0.Text = keyPad[birinciDeger, 0];
+                btn1.Text = keyPad[birinciDeger, 1];
+                btn2.Text = keyPad[birinciDeger, 2];
+                btn3.Text = keyPad[birinciDeger, 3];
+                btn4.Text = keyPad[birinciDeger, 4];
+                btn5.Text = keyPad[birinciDeger, 5];
+                btn6.Text = keyPad[birinciDeger, 6];
+                btn7.Text = keyPad[birinciDeger, 7];
+                activate(i, birinciDeger, ikinciDeger);
+            }
+            else
+            {
+                stepOne();
+                activate(i, birinciDeger, ikinciDeger);
+            }
+        }
+        private void stepOne()
+        {
+            btn0.Text = "X";
+            btn1.Text = "X";
+            btn2.Text = "X";
+            btn3.Text = "X";
+            btn4.Text = "X";
+            btn5.Text = "X";
+            btn6.Text = "X";
+            btn7.Text = "X";
+            btn8.Text = "<";
+        }
+        private void activate(bool sayac, int birinciDeger, int ikinciDeger)
+        {
+            if (sayac == false)
+            {
+                firstStep = birinciDeger;
+                i = true;
+            }
+            else
+            {
+                secondStep = ikinciDeger;
+                SendKeys.Send(keyPad[firstStep, secondStep]);
+                i = false;
+            }
+        }
+        private void centrelize()
+        {
+            btn8.Left = 150;
+            btn8.Top = 150;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //btn8.Left = 150;
+            //btn8.Top = 150;
+            //this.TransparencyKey = (BackColor);
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+        }
+
+        public Point MouseDownLocation;
+        private void ortak_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                MouseDownLocation = e.Location;
+            }
+        }
+
+        public void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int myXValue = btn8.Left - 150;//butonun değerleri x ve y ekseninde formun sol üst köşesindeki konumuna göre veriyor
+            int myYValue = btn8.Top - 150;//bende bu değerleri buton üzerinden sıfır alıp görmek istediğim için buton konumunu düştüm
+            double hipotenus = Math.Sqrt(myXValue * myXValue + myYValue * myYValue);// çapraz olanların merkeze uzaklığını aldık
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)//butona basılı olduğu sürece çalışıyor
+            {
+                btn8.Left = e.X + btn8.Left - MouseDownLocation.X;
+                btn8.Top = e.Y + btn8.Top - MouseDownLocation.Y;
+                buttonPos.Text = ("X = " + (myXValue) + "\nY = " + (myYValue) + "\nHipotenüs = " + hipotenus);
+            }
+            else//butona basmayı bırakınca buton orjinal konumuna dönüyorr.
+            {
+                centrelize();
+                buttonPos.Text = ("X = " + (btn8.Left - 150) + "\nY = " + (btn8.Top - 150) + "\nHipotenüs = 0");
+            }
+        }
+        private void btnSpace_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)//butona basılı olduğu sürece çalışıyor
+            {
+                btnSpace.Left = e.X + btnSpace.Left - MouseDownLocation.X;
+                btnSpace.Top = e.Y + btnSpace.Top - MouseDownLocation.Y;
+            }
+            else//butona basmayı bırakınca buton orjinal konumuna dönüyorr.
+            {
+                btnSpace.Left = 115;
+                btnSpace.Top = 320;
+            }
+        }
+        private void btnSpace_Click(object sender, EventArgs e)
+        {
+            if (btnSpace.Top <= 300)
+            {
+
+                SendKeys.Send(" ");
+            }
+            else if (btnSpace.Top >= 320 && btnSpace.Top <= 360 && btnSpace.Left >= 15 && btnSpace.Left <= 235)
+            {
+                SendKeys.Send(" ");
+            }
+        }
+
+        private void btnBackSpace_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)//butona basılı olduğu sürece çalışıyor
+            {
+                btnBackSpace.Left = e.X + btnBackSpace.Left - MouseDownLocation.X;
+                btnBackSpace.Top = e.Y + btnBackSpace.Top - MouseDownLocation.Y;
+            }
+            else//butona basmayı bırakınca buton orjinal konumuna dönüyorr.
+            {
+                btnBackSpace.Left = 320;
+                btnBackSpace.Top = 115;
+            }
+        }
+        private void btnBackSpace_Click(object sender, EventArgs e)
+        {
+            if (btnBackSpace.Left <= 300)
+            {
+
+                SendKeys.Send("{BS}");
+            }
+            else if (btnBackSpace.Left >= 320 && btnBackSpace.Left <= 360 && btnBackSpace.Top >= 15 && btnBackSpace.Top <= 235)
+            {
+                SendKeys.Send("{BS}");
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            int centreButtonX = btn8.Left + 25;//left değeri bize butonun merkezinden değil sol başlangıcından değer veriyor 
+            int centreButtonY = btn8.Top + 25;// top ta öyle, bende butonun yarısı kadar değer verip butonun merkezini simüle ediyorum.
+            if (btn8.Top >= 150 && btn8.Top <= 200 && btn8.Left >= 150 && btn8.Left <= 200)
+            {
+                if (i == false)
+                {
+                    writer(4, 9);
+                }
+                else
+                {
+                    stepOne();
+                    i = false;
+                }
+            }
+
+
+            else if (centreButtonY > btn1.Top && centreButtonY < (btn1.Top + 50) && centreButtonX > btn1.Left && centreButtonX < (btn1.Left + 50))
+            {
+                if (i == true)
+                {
+                    writer(1, 1);
+                }
+                else
+                {
+                    stepOne();
+                    i = false;
+                }
+            }
+            else if (centreButtonY > btn3.Top && centreButtonY < (btn3.Top + 50) && centreButtonX > btn3.Left && centreButtonX < (btn3.Left + 50))
+            {
+                if (i == true)
+                {
+                    writer(3, 3);
+                }
+                else
+                {
+                    stepOne();
+                    i = false;
+                }
+            }
+            else if (centreButtonY > btn5.Top && centreButtonY < (btn5.Top + 50) && centreButtonX > btn5.Left && centreButtonX < (btn5.Left + 50))
+            {
+                if (i == true)
+                {
+                    writer(5, 5);
+                }
+                else
+                {
+                    stepOne();
+                    i = false;
+                }
+            }
+            else if (centreButtonY > btn7.Top && centreButtonY < (btn7.Top + 50) && centreButtonX > btn7.Left && centreButtonX < (btn7.Left + 50))
+            {
+                if (i == true)
+                {
+                    writer(7, 7);
+                }
+                else
+                {
+                    stepOne();
+                    i = false;
+                }
+            }
+            else if (centreButtonY > btn6.Top && centreButtonY < (btn6.Top + 50) && centreButtonX > btn6.Left && centreButtonX < (btn6.Left + 50))
+            {
+                writer(3, 6);
+            }
+            else if (centreButtonY > btn4.Top && centreButtonY < (btn4.Top + 50) && centreButtonX > btn4.Left && centreButtonX < (btn4.Left + 50))
+            {
+                writer(2, 4);
+            }
+            else if (centreButtonY > btn2.Top && centreButtonY < (btn2.Top + 50) && centreButtonX > btn2.Left && centreButtonX < (btn2.Left + 50))
+            {
+                writer(1, 2);
+            }
+            else if (centreButtonY > btn0.Top && centreButtonY < (btn0.Top + 50) && centreButtonX > btn0.Left && centreButtonX < (btn0.Left + 50))
+            {
+                writer(0, 0);
+            }
+            else
+            {
+                centrelize();
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            writer(0, 0);
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                writer(1, 1);
+            }
+            else
+            {
+                stepOne();
+                i = false;
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            writer(1, 2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                writer(3, 3);
+            }
+            else
+            {
+                stepOne();
+                i = false;
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            writer(2, 4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                writer(5, 5);
+            }
+            else
+            {
+                stepOne();
+                i = false;
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            writer(3, 6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            if (i == true)
+            {
+                writer(7, 7);
+            }
+            else
+            {
+                stepOne();
+                i = false;
+            }
+        }
+        //mouse konumunu veriyor
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            //if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                base.OnMouseMove(e);
+                int x = e.X;
+                int y = e.Y;
+                mousePos.Text = "x degeri = " + x + " y degeri = " + y + " \nMouse Position = " + MousePosition.X + "\nmouse cos " + Math.Sqrt(y * y + x * x);
+                /*if (e.Button == MouseButtons.Left)
+                {
+                    btn0.Text = ("Mouse clicked");
+                }*/
+            }
+        }
+    }
+}
