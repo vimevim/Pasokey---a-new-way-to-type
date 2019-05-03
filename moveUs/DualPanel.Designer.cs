bd29a4f2cd1c363f0e1ab44d3c8046898e494072
Hasan SEZER <vimevim@users.noreply.github.com>
2019-05-03 06:28:07 +0300

    güncelleme

--- moveUs/DualPanel.Designer.cs
+++ moveUs/DualPanel.Designer.cs
@@ -30,17 +30,6 @@
         {
             this.btnLeftUniter = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.leftGuide = new moveUs.ButtonElipse();
-            this.btn3 = new moveUs.ButtonElipse();
-            this.btn2 = new moveUs.ButtonElipse();
-            this.btn1 = new moveUs.ButtonElipse();
-            this.btn4 = new moveUs.ButtonElipse();
-            this.btn0 = new moveUs.ButtonElipse();
-            this.btn5 = new moveUs.ButtonElipse();
-            this.btn6 = new moveUs.ButtonElipse();
-            this.btn7 = new moveUs.ButtonElipse();
-            this.buttonElipse1 = new moveUs.ButtonElipse();
-            this.btnUpper = new moveUs.ButtonElipse();
             this.btnLeftDivider = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnBackSpace = new moveUs.ButtonElipse();
@@ -55,8 +44,17 @@
             this.btnLbl7 = new moveUs.ButtonElipse();
             this.buttonElipse11 = new moveUs.ButtonElipse();
             this.btnSpace = new moveUs.ButtonElipse();
-            this.btnRightUniter = new System.Windows.Forms.Button();
-            this.btnRightDivider = new System.Windows.Forms.Button();
+            this.leftGuide = new moveUs.ButtonElipse();
+            this.btn3 = new moveUs.ButtonElipse();
+            this.btn2 = new moveUs.ButtonElipse();
+            this.btn1 = new moveUs.ButtonElipse();
+            this.btn4 = new moveUs.ButtonElipse();
+            this.btn0 = new moveUs.ButtonElipse();
+            this.btn5 = new moveUs.ButtonElipse();
+            this.btn6 = new moveUs.ButtonElipse();
+            this.btn7 = new moveUs.ButtonElipse();
+            this.buttonElipse1 = new moveUs.ButtonElipse();
+            this.btnUpper = new moveUs.ButtonElipse();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -95,141 +93,6 @@
             this.panel1.Size = new System.Drawing.Size(350, 350);
             this.panel1.TabIndex = 4;
             // 
-            // leftGuide
-            // 
-            this.leftGuide.FlatAppearance.BorderSize = 0;
-            this.leftGuide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.leftGuide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.leftGuide.Location = new System.Drawing.Point(150, 150);
-            this.leftGuide.Name = "leftGuide";
-            this.leftGuide.Size = new System.Drawing.Size(50, 50);
-            this.leftGuide.TabIndex = 53;
-            this.leftGuide.Text = "X";
-            this.leftGuide.UseVisualStyleBackColor = true;
-            this.leftGuide.Click += new System.EventHandler(this.leftGuide_Click);
-            this.leftGuide.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
-            this.leftGuide.MouseMove += new System.Windows.Forms.MouseEventHandler(this.leftGuide_MouseMove);
-            this.leftGuide.MouseUp += new System.Windows.Forms.MouseEventHandler(this.general_MouseUp);
-            // 
-            // btn3
-            // 
-            this.btn3.Enabled = false;
-            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn3.Location = new System.Drawing.Point(220, 220);
-            this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(50, 50);
-            this.btn3.TabIndex = 48;
-            this.btn3.Text = "X";
-            this.btn3.UseVisualStyleBackColor = true;
-            // 
-            // btn2
-            // 
-            this.btn2.Enabled = false;
-            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn2.Location = new System.Drawing.Point(250, 150);
-            this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(50, 50);
-            this.btn2.TabIndex = 49;
-            this.btn2.Text = "X";
-            this.btn2.UseVisualStyleBackColor = true;
-            // 
-            // btn1
-            // 
-            this.btn1.Enabled = false;
-            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn1.Location = new System.Drawing.Point(220, 80);
-            this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(50, 50);
-            this.btn1.TabIndex = 50;
-            this.btn1.Text = "X";
-            this.btn1.UseVisualStyleBackColor = true;
-            // 
-            // btn4
-            // 
-            this.btn4.Enabled = false;
-            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn4.Location = new System.Drawing.Point(150, 250);
-            this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(50, 50);
-            this.btn4.TabIndex = 51;
-            this.btn4.Text = "X";
-            this.btn4.UseVisualStyleBackColor = true;
-            // 
-            // btn0
-            // 
-            this.btn0.Enabled = false;
-            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn0.Location = new System.Drawing.Point(150, 50);
-            this.btn0.Name = "btn0";
-            this.btn0.Size = new System.Drawing.Size(50, 50);
-            this.btn0.TabIndex = 52;
-            this.btn0.Text = "X";
-            this.btn0.UseVisualStyleBackColor = true;
-            // 
-            // btn5
-            // 
-            this.btn5.Enabled = false;
-            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn5.Location = new System.Drawing.Point(80, 220);
-            this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(50, 50);
-            this.btn5.TabIndex = 45;
-            this.btn5.Text = "X";
-            this.btn5.UseVisualStyleBackColor = true;
-            // 
-            // btn6
-            // 
-            this.btn6.Enabled = false;
-            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn6.Location = new System.Drawing.Point(50, 150);
-            this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(50, 50);
-            this.btn6.TabIndex = 46;
-            this.btn6.Text = "X";
-            this.btn6.UseVisualStyleBackColor = true;
-            // 
-            // btn7
-            // 
-            this.btn7.Enabled = false;
-            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btn7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btn7.Location = new System.Drawing.Point(80, 80);
-            this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(50, 50);
-            this.btn7.TabIndex = 47;
-            this.btn7.Text = "X";
-            this.btn7.UseVisualStyleBackColor = true;
-            // 
-            // buttonElipse1
-            // 
-            this.buttonElipse1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.buttonElipse1.Enabled = false;
-            this.buttonElipse1.Location = new System.Drawing.Point(25, 25);
-            this.buttonElipse1.Name = "buttonElipse1";
-            this.buttonElipse1.Size = new System.Drawing.Size(300, 300);
-            this.buttonElipse1.TabIndex = 44;
-            this.buttonElipse1.UseVisualStyleBackColor = false;
-            // 
-            // btnUpper
-            // 
-            this.btnUpper.Location = new System.Drawing.Point(115, 320);
-            this.btnUpper.Name = "btnUpper";
-            this.btnUpper.Size = new System.Drawing.Size(120, 40);
-            this.btnUpper.TabIndex = 42;
-            this.btnUpper.Text = "upper";
-            this.btnUpper.UseVisualStyleBackColor = true;
-            this.btnUpper.Click += new System.EventHandler(this.btnUpper_Click);
-            this.btnUpper.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
-            this.btnUpper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnUpper_MouseMove);
-            this.btnUpper.MouseUp += new System.Windows.Forms.MouseEventHandler(this.general_MouseUp);
-            // 
             // btnLeftDivider
             // 
             this.btnLeftDivider.Location = new System.Drawing.Point(0, 250);
@@ -256,8 +119,6 @@
             this.panel2.Controls.Add(this.btnLbl7);
             this.panel2.Controls.Add(this.buttonElipse11);
             this.panel2.Controls.Add(this.btnSpace);
-            this.panel2.Controls.Add(this.btnRightUniter);
-            this.panel2.Controls.Add(this.btnRightDivider);
             this.panel2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.panel2.Location = new System.Drawing.Point(350, 0);
             this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
@@ -413,25 +274,140 @@
             this.btnSpace.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnSpace_MouseMove);
             this.btnSpace.MouseUp += new System.Windows.Forms.MouseEventHandler(this.general_MouseUp);
             // 
-            // btnRightUniter
+            // leftGuide
             // 
-            this.btnRightUniter.Location = new System.Drawing.Point(0, 250);
-            this.btnRightUniter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnRightUniter.Name = "btnRightUniter";
-            this.btnRightUniter.Size = new System.Drawing.Size(20, 100);
-            this.btnRightUniter.TabIndex = 40;
-            this.btnRightUniter.Text = "<<<";
-            this.btnRightUniter.UseVisualStyleBackColor = true;
+            this.leftGuide.FlatAppearance.BorderSize = 0;
+            this.leftGuide.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.leftGuide.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.leftGuide.Location = new System.Drawing.Point(150, 150);
+            this.leftGuide.Name = "leftGuide";
+            this.leftGuide.Size = new System.Drawing.Size(50, 50);
+            this.leftGuide.TabIndex = 53;
+            this.leftGuide.Text = "X";
+            this.leftGuide.UseVisualStyleBackColor = true;
+            this.leftGuide.Click += new System.EventHandler(this.leftGuide_Click);
+            this.leftGuide.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
+            this.leftGuide.MouseMove += new System.Windows.Forms.MouseEventHandler(this.leftGuide_MouseMove);
+            this.leftGuide.MouseUp += new System.Windows.Forms.MouseEventHandler(this.general_MouseUp);
             // 
-            // btnRightDivider
+            // btn3
             // 
-            this.btnRightDivider.Location = new System.Drawing.Point(331, 250);
-            this.btnRightDivider.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnRightDivider.Name = "btnRightDivider";
-            this.btnRightDivider.Size = new System.Drawing.Size(20, 100);
-            this.btnRightDivider.TabIndex = 41;
-            this.btnRightDivider.Text = ">>>";
-            this.btnRightDivider.UseVisualStyleBackColor = true;
+            this.btn3.Enabled = false;
+            this.btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn3.Location = new System.Drawing.Point(220, 220);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(50, 50);
+            this.btn3.TabIndex = 48;
+            this.btn3.Text = "X";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn2.Location = new System.Drawing.Point(250, 150);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(50, 50);
+            this.btn2.TabIndex = 49;
+            this.btn2.Text = "X";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Enabled = false;
+            this.btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn1.Location = new System.Drawing.Point(220, 80);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(50, 50);
+            this.btn1.TabIndex = 50;
+            this.btn1.Text = "X";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Enabled = false;
+            this.btn4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn4.Location = new System.Drawing.Point(150, 250);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(50, 50);
+            this.btn4.TabIndex = 51;
+            this.btn4.Text = "X";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Enabled = false;
+            this.btn0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn0.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn0.Location = new System.Drawing.Point(150, 50);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(50, 50);
+            this.btn0.TabIndex = 52;
+            this.btn0.Text = "X";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Enabled = false;
+            this.btn5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn5.Location = new System.Drawing.Point(80, 220);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(50, 50);
+            this.btn5.TabIndex = 45;
+            this.btn5.Text = "X";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Enabled = false;
+            this.btn6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn6.Location = new System.Drawing.Point(50, 150);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(50, 50);
+            this.btn6.TabIndex = 46;
+            this.btn6.Text = "X";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Enabled = false;
+            this.btn7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn7.Location = new System.Drawing.Point(80, 80);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(50, 50);
+            this.btn7.TabIndex = 47;
+            this.btn7.Text = "X";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // buttonElipse1
+            // 
+            this.buttonElipse1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.buttonElipse1.Enabled = false;
+            this.buttonElipse1.Location = new System.Drawing.Point(25, 25);
+            this.buttonElipse1.Name = "buttonElipse1";
+            this.buttonElipse1.Size = new System.Drawing.Size(300, 300);
+            this.buttonElipse1.TabIndex = 44;
+            this.buttonElipse1.UseVisualStyleBackColor = false;
+            // 
+            // btnUpper
+            // 
+            this.btnUpper.Location = new System.Drawing.Point(115, 320);
+            this.btnUpper.Name = "btnUpper";
+            this.btnUpper.Size = new System.Drawing.Size(120, 40);
+            this.btnUpper.TabIndex = 42;
+            this.btnUpper.Text = "upper";
+            this.btnUpper.UseVisualStyleBackColor = true;
+            this.btnUpper.Click += new System.EventHandler(this.btnUpper_Click);
+            this.btnUpper.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ortak_MouseDown);
+            this.btnUpper.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnUpper_MouseMove);
+            this.btnUpper.MouseUp += new System.Windows.Forms.MouseEventHandler(this.general_MouseUp);
             // 
             // DualPanel
             // 
@@ -479,8 +455,6 @@
         private ButtonElipse btnLbl7;
         private ButtonElipse buttonElipse11;
         private ButtonElipse btnSpace;
-        private System.Windows.Forms.Button btnRightUniter;
-        private System.Windows.Forms.Button btnRightDivider;
         private ButtonElipse btnBackSpace;
     }
 }
